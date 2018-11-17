13e99d1fb004cf99937006157f90bf81b5d223b2
Michael Roose <mjroose@gmail.com>
2018-11-17 14:08:57 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,8 +1,8 @@
-class CreateSchoolClasses < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :school_classes do |t|
-      t.string :title
-      t.integer :room_number
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps null: false
     end
