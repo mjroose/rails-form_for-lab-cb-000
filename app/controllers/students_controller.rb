e5b4ca41161c9c2e82edbc8f860a05add7842a06
Michael Roose <mjroose@gmail.com>
2018-11-17 14:23:46 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,23 +4,27 @@ class StudentsController < ApplicationController
   end
 
   def show
-
+    @student = Student.find(params[:id])
   end
 
   def new
-
+    @student = Student.new
   end
 
   def create
-
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def edit
-
+    @student = Student.find(params[:id])
   end
 
   def update
-
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   private
