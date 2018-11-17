dea39fb30057dc2e74f102e5ac5c1d989081d075
Michael Roose <mjroose@gmail.com>
2018-11-17 14:16:12 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,31 @@
 class StudentsController < ApplicationController
+  def index
 
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+    def student_params(*args)
+      params.require(:student).permit(*args)
+    end
 end
